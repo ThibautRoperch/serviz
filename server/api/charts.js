915f6a6d66a457e307b74446c79da973d43e8f09
ThibautRoperch <thibaut.roperch@free.fr>
2019-12-08 02:27:38 +0100

    Structure update

--- server/api/charts.js
+++ server/api/charts.js
@@ -6,30 +6,50 @@ const router = express.Router()
 // Get charts
 router.get('/', async (req, res) => {
   const charts = await loadchartsCollection()
-  res.send(await charts.find({}).sort({ $natural: -1 }).toArray())
+  try {
+    let limit = parseInt(req.query.limit) || 10
+    res.send(await charts.find({}).sort({ $natural: -1 }).limit(limit).toArray())
+  } catch (e) {
+    res.send("Unable to load the collection")
+  }
 })
 
 // Add chart
 router.post('/', async (req, res) => {
   const charts = await loadchartsCollection()
-  await charts.insertOne({
-    ...req.body,
-    createdAt: new Date()
-  })
-  res.status(201).send()
+  try {
+    await charts.insertOne({
+      ...req.body,
+      createdAt: new Date()
+    })
+    res.status(201).send()
+  } catch (e) {
+    res.send("Unable to load the collection")
+  }
 })
 
 // Delete chart
 router.delete('/:id', async (req, res) => {
   const charts = await loadchartsCollection()
-  await charts.deleteOne({ _id: new mongodb.ObjectID(req.params.id) })
-  res.status(200).send()
+  try {
+    await charts.deleteOne({ _id: new mongodb.ObjectID(req.params.id) })
+    res.status(200).send()
+  } catch (e) {
+    res.send("Unable to load the collection")
+  }
 })
 
 async function loadchartsCollection() {
-  const client = await mongodb.MongoClient.connect('mongodb://localhost:32769/db/serviz', {
-    useNewUrlParser: true
-  })
+  let client
+
+  try {
+    client = await mongodb.MongoClient.connect('mongodb://localhost:32774/', {
+      useNewUrlParser: true
+    })
+  } catch (e) {
+    console.log('MongoClient connection error : ' + e.name)
+    return null
+  }
 
   return client.db('serviz').collection('charts')
 }
